067b611f535c0fdad6acaa8dd18089b3ef518cec
Jonathan-Mira <123763590+Jonathan-Mira@users.noreply.github.com>
2025-03-07 11:56:34 -0800

    _DataLog is complete enough

--- DataLog.py
+++ DataLog.py
@@ -35,158 +35,158 @@ class _Datalog:
         self._CSVWriter = csv.writer(self._CSVFile)
         
         #Creates the Headers for the CSV File
-        self._CSVWriter.writerow(['Date', 'Time'\
-                                    """General"""\
+        self._CSVWriter.writerow(['Date', 'Time',\
+                                    \
                                         'Emergency Stop', 'Windshield Wiper', 'Drive Mode',\
                                         \
-                                    """Velocity Variables"""\
+                                    \
                                         'Throttle', 'Brake', 'Acceleration', 'RPM', 'Speed', 'Distance',\
                                         \
-                                    """Motor Signals"""\
+                                    \
                                         'Controller Temp 1', 'Controller Temp 2', 'Controller Temp 3',\
                                         'Controller Temp 4', 'Controller Temp 5', 'Controller Temp 6',\
                                         'Motor Temp', 'Phase Current A', 'Phase Current B', 'Phase Current C', 'Motor Torque',\
                                         \
-                                    """Motor Vector Signals"""\
+                                    \
                                         'Motor Vector 1', 'Motor Vector 2', 'Motor Vector 3',\
                                         'Motor Vector 4', 'Motor Vector 5', 'Motor Vector 6',\
                                         \
-                                    """Battery Signals"""\
+                                    \
                                         'Battery Current', 'BMS 1 Status', 'BMS 2 Status',\
                                         \
-                                    """Battery 1"""\
+                                    \
                                         'Battery 1 Capacity', 'Battery 1 Temp 1', 'Battery 1 Temp 2',\
                                         'Battery 1 Voltage 1', 'Battery 1 Voltage 2', 'Battery 1 Voltage 3', 'Battery 1 Voltage 4',\
                                         'Battery 1 Voltage 5', 'Battery 1 Voltage 6', 'Battery 1 Voltage 7', 'Battery 1 Voltage 8',\
                                         \
-                                    """Battery 2"""\
+                                    \
                                         'Battery 2 Capacity', 'Battery 2 Temp 1', 'Battery 2 Temp 2',\
                                         'Battery 2 Voltage 1', 'Battery 2 Voltage 2', 'Battery 2 Voltage 3', 'Battery 2 Voltage 4',\
                                         'Battery 2 Voltage 5', 'Battery 2 Voltage 6', 'Battery 2 Voltage 7', 'Battery 2 Voltage 8',\
                                         \
-                                    """Battery 3"""\
+                                    \
                                         'Battery 3 Capacity', 'Battery 3 Temp 1', 'Battery 3 Temp 2',\
                                         'Battery 3 Voltage 1', 'Battery 3 Voltage 2', 'Battery 3 Voltage 3', 'Battery 3 Voltage 4',\
                                         'Battery 3 Voltage 5', 'Battery 3 Voltage 6', 'Battery 3 Voltage 7', 'Battery 3 Voltage 8',\
                                         \
-                                    """Battery 4"""\
+                                    \
                                         'Battery 4 Capacity', 'Battery 4 Temp 1', 'Battery 4 Temp 2',\
                                         'Battery 4 Voltage 1', 'Battery 4 Voltage 2', 'Battery 4 Voltage 3', 'Battery 4 Voltage 4',\
                                         'Battery 4 Voltage 5', 'Battery 4 Voltage 6', 'Battery 4 Voltage 7', 'Battery 4 Voltage 8',\
                                         \
-                                    """Battery 5"""\
+                                    \
                                         'Battery 5 Capacity', 'Battery 5 Temp 1', 'Battery 5 Temp 2',\
                                         'Battery 5 Voltage 1', 'Battery 5 Voltage 2', 'Battery 5 Voltage 3', 'Battery 5 Voltage 4',\
                                         'Battery 5 Voltage 5', 'Battery 5 Voltage 6', 'Battery 5 Voltage 7', 'Battery 5 Voltage 8',\
                                         \
-                                    """Battery 6"""\
+                                    \
                                         'Battery 6 Capacity', 'Battery 6 Temp 1', 'Battery 6 Temp 2',\
                                         'Battery 6 Voltage 1', 'Battery 6 Voltage 2', 'Battery 6 Voltage 3', 'Battery 6 Voltage 4',\
                                         'Battery 6 Voltage 5', 'Battery 6 Voltage 6', 'Battery 6 Voltage 7', 'Battery 6 Voltage 8'\
-                                    ])
+                                ])
         #Addes the Variable ID to the CSV File
         self._CSVWriter.writerow(['', '',\
-                                  """General""",\
-                                    self._EmergencyStop._ID, self._WindShieldWiper._ID, self._DriveMode._ID,\
                                     \
-                                """Velocity Variables""",\
-                                    self._Throttle._ID, self._Brake._ID, self._Acceleration._ID, self._RPM._ID, self._Speed._ID, self._Distance._ID,\
+                                        self._EmergencyStop._ID, self._WindShieldWiper._ID, self._DriveMode._ID,\
+                                        \
                                     \
-                                """Motor Signals""",\
-                                    self._ControllerTemp1._ID, self._ControllerTemp2._ID, self._ControllerTemp3._ID,\
-                                    self._ControllerTemp4._ID, self._ControllerTemp5._ID, self._ControllerTemp6._ID,\
-                                    self._MotorTemp._ID, self._PhaseCurrentA._ID, self._PhaseCurrentB._ID, self._PhaseCurrentC._ID, self._MotorTorque._ID,\
+                                        self._Throttle._ID, self._Brake._ID, self._Acceleration._ID, self._RPM._ID, self._Speed._ID, self._Distance._ID,\
+                                        \
                                     \
-                                """Motor Vector Signals""",\
-                                    self._MotorVector1._ID, self._MotorVector2._ID, self._MotorVector3._ID,\
-                                    self._MotorVector4._ID, self._MotorVector5._ID, self._MotorVector6._ID,\
+                                        self._ControllerTemp1._ID, self._ControllerTemp2._ID, self._ControllerTemp3._ID,\
+                                        self._ControllerTemp4._ID, self._ControllerTemp5._ID, self._ControllerTemp6._ID,\
+                                        self._MotorTemp._ID, self._PhaseCurrentA._ID, self._PhaseCurrentB._ID, self._PhaseCurrentC._ID, self._MotorTorque._ID,\
+                                        \
                                     \
-                                """Battery Signals""",\
-                                    self._BatteryCurrent._ID, self._BMS1Status._ID, self._BMS2Status._ID,\
+                                        self._MotorVector1._ID, self._MotorVector2._ID, self._MotorVector3._ID,\
+                                        self._MotorVector4._ID, self._MotorVector5._ID, self._MotorVector6._ID,\
+                                        \
                                     \
-                                """Battery 1""",\
-                                    self._Battery1._Capacity._ID, self._Battery1._Temp1._ID, self._Battery1._Temp2._ID,\
-                                    self._Battery1._Voltage1._ID, self._Battery1._Voltage2._ID, self._Battery1._Voltage3._ID, self._Battery1._Voltage4._ID,\
-                                    self._Battery1._Voltage5._ID, self._Battery1._Voltage6._ID, self._Battery1._Voltage7._ID, self._Battery1._Voltage8._ID,\
+                                        self._BatteryCurrent._ID, self._BMS1Status._ID, self._BMS2Status._ID,\
+                                        \
                                     \
-                                """Battery 2""",\
-                                    self._Battery2._Capacity._ID, self._Battery2._Temp1._ID, self._Battery2._Temp2._ID,\
-                                    self._Battery2._Voltage1._ID, self._Battery2._Voltage2._ID, self._Battery2._Voltage3._ID, self._Battery2._Voltage4._ID,\
-                                    self._Battery2._Voltage5._ID, self._Battery2._Voltage6._ID, self._Battery2._Voltage7._ID, self._Battery2._Voltage8._ID,\
+                                        self._Battery1._Capacity._ID, self._Battery1._Temp1._ID, self._Battery1._Temp2._ID,\
+                                        self._Battery1._Voltage1._ID, self._Battery1._Voltage2._ID, self._Battery1._Voltage3._ID, self._Battery1._Voltage4._ID,\
+                                        self._Battery1._Voltage5._ID, self._Battery1._Voltage6._ID, self._Battery1._Voltage7._ID, self._Battery1._Voltage8._ID,\
+                                        \
                                     \
-                                """Battery 3""",\
-                                    self._Battery3._Capacity._ID, self._Battery3._Temp1._ID, self._Battery3._Temp2._ID,\
-                                    self._Battery3._Voltage1._ID, self._Battery3._Voltage2._ID, self._Battery3._Voltage3._ID, self._Battery3._Voltage4._ID,\
-                                    self._Battery3._Voltage5._ID, self._Battery3._Voltage6._ID, self._Battery3._Voltage7._ID, self._Battery3._Voltage8._ID,\
+                                        self._Battery2._Capacity._ID, self._Battery2._Temp1._ID, self._Battery2._Temp2._ID,\
+                                        self._Battery2._Voltage1._ID, self._Battery2._Voltage2._ID, self._Battery2._Voltage3._ID, self._Battery2._Voltage4._ID,\
+                                        self._Battery2._Voltage5._ID, self._Battery2._Voltage6._ID, self._Battery2._Voltage7._ID, self._Battery2._Voltage8._ID,\
+                                        \
                                     \
-                                """Battery 4""",\
-                                    self._Battery4._Capacity._ID, self._Battery4._Temp1._ID, self._Battery4._Temp2._ID,\
-                                    self._Battery4._Voltage1._ID, self._Battery4._Voltage2._ID, self._Battery4._Voltage3._ID, self._Battery4._Voltage4._ID,\
-                                    self._Battery4._Voltage5._ID, self._Battery4._Voltage6._ID, self._Battery4._Voltage7._ID, self._Battery4._Voltage8._ID,\
+                                        self._Battery3._Capacity._ID, self._Battery3._Temp1._ID, self._Battery3._Temp2._ID,\
+                                        self._Battery3._Voltage1._ID, self._Battery3._Voltage2._ID, self._Battery3._Voltage3._ID, self._Battery3._Voltage4._ID,\
+                                        self._Battery3._Voltage5._ID, self._Battery3._Voltage6._ID, self._Battery3._Voltage7._ID, self._Battery3._Voltage8._ID,\
+                                        \
                                     \
-                                """Battery 5""",\
-                                    self._Battery5._Capacity._ID, self._Battery5._Temp1._ID, self._Battery5._Temp2._ID,\
-                                    self._Battery5._Voltage1._ID, self._Battery5._Voltage2._ID, self._Battery5._Voltage3._ID, self._Battery5._Voltage4._ID,\
-                                    self._Battery5._Voltage5._ID, self._Battery5._Voltage6._ID, self._Battery5._Voltage7._ID, self._Battery5._Voltage8._ID,\
+                                        self._Battery4._Capacity._ID, self._Battery4._Temp1._ID, self._Battery4._Temp2._ID,\
+                                        self._Battery4._Voltage1._ID, self._Battery4._Voltage2._ID, self._Battery4._Voltage3._ID, self._Battery4._Voltage4._ID,\
+                                        self._Battery4._Voltage5._ID, self._Battery4._Voltage6._ID, self._Battery4._Voltage7._ID, self._Battery4._Voltage8._ID,\
+                                        \
                                     \
-                                """Battery 6""",\
-                                    self._Battery6._Capacity._ID, self._Battery6._Temp1._ID, self._Battery6._Temp2._ID,\
-                                    self._Battery6._Voltage1._ID, self._Battery6._Voltage2._ID, self._Battery6._Voltage3._ID, self._Battery6._Voltage4._ID,\
-                                    self._Battery6._Voltage5._ID, self._Battery6._Voltage6._ID, self._Battery6._Voltage7._ID, self._Battery6._Voltage8._ID\
+                                        self._Battery5._Capacity._ID, self._Battery5._Temp1._ID, self._Battery5._Temp2._ID,\
+                                        self._Battery5._Voltage1._ID, self._Battery5._Voltage2._ID, self._Battery5._Voltage3._ID, self._Battery5._Voltage4._ID,\
+                                        self._Battery5._Voltage5._ID, self._Battery5._Voltage6._ID, self._Battery5._Voltage7._ID, self._Battery5._Voltage8._ID,\
+                                        \
+                                    \
+                                        self._Battery6._Capacity._ID, self._Battery6._Temp1._ID, self._Battery6._Temp2._ID,\
+                                        self._Battery6._Voltage1._ID, self._Battery6._Voltage2._ID, self._Battery6._Voltage3._ID, self._Battery6._Voltage4._ID,\
+                                        self._Battery6._Voltage5._ID, self._Battery6._Voltage6._ID, self._Battery6._Voltage7._ID, self._Battery6._Voltage8._ID\
                                 ])
 
         
 
     def _FileWrite(self): #Write the data to the file
         self._CSVWriter.writerow([self._DateTime.strftime('%y/%m/%d'), self._DateTime.strftime('%H:%M:%S.%f'),\
-                                    """General""",\
+                                    \
                                         self._EmergencyStop._value, self._WindShieldWiper._value, self._DriveMode._value,\
                                         \
-                                    """Velocity Variables""",\
+                                    \
                                         self._Throttle._value, self._Brake._value, self._Acceleration._value, self._RPM._value, self._Speed._value, self._Distance._value,\
                                         \
-                                    """Motor Signals""",\
+                                    \
                                         self._ControllerTemp1._value, self._ControllerTemp2._value, self._ControllerTemp3._value,\
                                         self._ControllerTemp4._value, self._ControllerTemp5._value, self._ControllerTemp6._value,\
                                         self._MotorTemp._value, self._PhaseCurrentA._value, self._PhaseCurrentB._value, self._PhaseCurrentC._value, self._MotorTorque._value,\
                                         \
-                                    """Motor Vector Signals""",\
+                                    \
                                         self._MotorVector1._value, self._MotorVector2._value, self._MotorVector3._value,\
                                         self._MotorVector4._value, self._MotorVector5._value, self._MotorVector6._value,\
                                         \
-                                    """Battery Signals""",\
+                                    \
                                         self._BatteryCurrent._value, self._BMS1Status._value, self._BMS2Status._value,\
                                         \
-                                    """Battery 1""",\
+                                    \
                                         self._Battery1._Capacity._value, self._Battery1._Temp1._value, self._Battery1._Temp2._value,\
                                         self._Battery1._Voltage1._value, self._Battery1._Voltage2._value, self._Battery1._Voltage3._value, self._Battery1._Voltage4._value,\
                                         self._Battery1._Voltage5._value, self._Battery1._Voltage6._value, self._Battery1._Voltage7._value, self._Battery1._Voltage8._value,\
                                         \
-                                    """Battery 2""",\
+                                    \
                                         self._Battery2._Capacity._value, self._Battery2._Temp1._value, self._Battery2._Temp2._value,\
                                         self._Battery2._Voltage1._value, self._Battery2._Voltage2._value, self._Battery2._Voltage3._value, self._Battery2._Voltage4._value,\
                                         self._Battery2._Voltage5._value, self._Battery2._Voltage6._value, self._Battery2._Voltage7._value, self._Battery2._Voltage8._value,\
                                         \
-                                    """Battery 3""",\
+                                    \
                                         self._Battery3._Capacity._value, self._Battery3._Temp1._value, self._Battery3._Temp2._value,\
                                         self._Battery3._Voltage1._value, self._Battery3._Voltage2._value, self._Battery3._Voltage3._value, self._Battery3._Voltage4._value,\
                                         self._Battery3._Voltage5._value, self._Battery3._Voltage6._value, self._Battery3._Voltage7._value, self._Battery3._Voltage8._value,\
                                         \
-                                    """Battery 4""",\
+                                    \
                                         self._Battery4._Capacity._value, self._Battery4._Temp1._value, self._Battery4._Temp2._value,\
                                         self._Battery4._Voltage1._value, self._Battery4._Voltage2._value, self._Battery4._Voltage3._value, self._Battery4._Voltage4._value,\
                                         self._Battery4._Voltage5._value, self._Battery4._Voltage6._value, self._Battery4._Voltage7._value, self._Battery4._Voltage8._value,\
                                         \
-                                    """Battery 5""",\
+                                    \
                                         self._Battery5._Capacity._value, self._Battery5._Temp1._value, self._Battery5._Temp2._value,\
                                         self._Battery5._Voltage1._value, self._Battery5._Voltage2._value, self._Battery5._Voltage3._value, self._Battery5._Voltage4._value,\
                                         self._Battery5._Voltage5._value, self._Battery5._Voltage6._value, self._Battery5._Voltage7._value, self._Battery5._Voltage8._value,\
                                         \
-                                    """Battery 6""",\
+                                    \
                                         self._Battery6._Capacity._value, self._Battery6._Temp1._value, self._Battery6._Temp2._value,\
                                         self._Battery6._Voltage1._value, self._Battery6._Voltage2._value, self._Battery6._Voltage3._value, self._Battery6._Voltage4._value,\
                                         self._Battery6._Voltage5._value, self._Battery6._Voltage6._value, self._Battery6._Voltage7._value, self._Battery6._Voltage8._value\
-                                    ])
+                                ])
         
     def _FileClose(self): #Close the file
         self._CSVFile.close()
